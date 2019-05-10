d1f292b4e1b0068dcd7d7dd452a7fcfe5447b496
“chhylp123” <chhy@mail.ustc.edu.cn>
2019-05-10 16:14:01 -0400

    Using klib

--- CommandLines.cpp
+++ CommandLines.cpp
@@ -1,7 +1,7 @@
 #include "CommandLines.h"
 #include <stdlib.h>
-#include <getopt.h>
 #include <stdio.h>
+#include "ketopt.h"
 
 char* read_file_name = NULL;
 char* output_file_name = NULL;
@@ -17,40 +17,27 @@ void Print_H()
 
 int CommandLine_process (int argc, char *argv[])
 {
-    int o; 
-    int index;
 
-    static struct option longOptions[] =
-    {
-        {"seq",		required_argument, 0, 'q'},
-        {"threads",	required_argument, 0, 't'},
-        {"threads",	required_argument, 0, 'o'},
-        {0,  0,  0, 0},
+    static ko_longopt_t longopts[] = {
+        { "help", ko_no_argument,       100},
+        { "seq", ko_required_argument, 101},
+        { "output", ko_required_argument, 102},
+        { "thread", ko_required_argument, 103},
+        { NULL, 0, 0 }
     };
 
+    ketopt_t opt = KETOPT_INIT;
 
-    while ( (o = getopt_long ( argc, argv, "q:t:o:", longOptions, &index)) != -1 )
-    {
-        switch (o)
-        {
-            case 'q':
-                read_file_name = optarg;
-                break;
-            case 'o':
-                output_file_name = optarg;
-                break;
-            case 't':
-                thread_num = atoi(optarg);
-                break;
-            case '?':
-                fprintf(stderr, "Unrecognized option!\n" );
-                return 0;   
-                break;
-            default:
-                Print_H();
-                return 0;   
-        }
+    int i, c;
+    while ((c = ketopt(&opt, argc, argv, 1, "ht:o:q:", longopts)) >= 0) {
+        if (c == 100 || c == 'h') Print_H();
+        else if (c == 103 || c == 't') thread_num = atoi(opt.arg); 
+        else if (c == 102 || c == 'o') output_file_name = opt.arg;
+        else if (c == 101 || c == 'q') read_file_name = opt.arg;
+        else if (c == '?') printf("unknown opt: -%c\n", opt.opt? opt.opt : ':');
+        else if (c == ':') printf("missing arg: -%c\n", opt.opt? opt.opt : ':');
     }
+
 
     if (argc == 1)
     {
--- Process_Read.h
+++ Process_Read.h
@@ -4,73 +4,15 @@
 #include<stdint.h>
 #include <string.h>
 #include <stdlib.h>
+#include <zlib.h>
+#include "kseq.h"
 
 
-#define FASTQ 1
-#define FASTQGZ 2
+KSEQ_INIT(gzFile, gzread)
 
-#define NAME_SZIE 25
-#define SEQ_SZIE 20000
+void init_kseq(char* file);
+void destory_kseq();
+int get_read(kseq_t *s);
 
-
-typedef struct
-{
-	char* centext;
-	uint32_t length;
-	uint32_t size;
-
-}str_v;
-
-
-inline void init_str_v(str_v* buf, uint32_t length)
-{
-	buf->size = length + 1;
-	buf->length = 0;
-	buf->centext = (char*)malloc(sizeof(char)*buf->size);
-}
-
-
-inline void append_str(str_v* buf, char* input)
-{
-	int length = strlen(input);
-	buf->length = buf->length + length;
-	if (buf->length > buf->size)
-	{
-		buf->centext = (char*)realloc(buf->centext, buf->length + 1);
-	}
-	memcpy(buf->centext + buf->length - length, input, length);
-	buf->centext[buf->length] = '\0';
-}
-
-inline void clear_str(str_v* buf)
-{
-	buf->length = 0;
-}
-
-typedef struct
-{
-	str_v name;
-	str_v seq;
-	str_v qual;
-
-} Read;
-
-inline void init_Read(Read* r)
-{
-	init_str_v(&r->name, NAME_SZIE);
-	init_str_v(&r->seq, SEQ_SZIE);
-	init_str_v(&r->qual, SEQ_SZIE);
-}
-
-inline void clear_read(Read* r)
-{
-	clear_str(&r->name);
-	clear_str(&r->seq);
-	clear_str(&r->qual);
-}
-
-
-int initiReadAllReads(char *fileName);
-int inputRead(Read *read);
 
 #endif
