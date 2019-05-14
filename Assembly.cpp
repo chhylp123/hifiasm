ee1234c209aace1ce56afc5cd1b5dd44ba1b09ee
“chhylp123” <chhy@mail.ustc.edu.cn>
2019-05-14 17:02:03 -0400

    Update position hash table

--- Assembly.cpp
+++ Assembly.cpp
@@ -8,73 +8,8 @@
 #include "Hash_Table.h"
 
 Total_Count_Table TCB;
-
-
-
-/********************************for debug***************************************/
-void Verify_Counting()
-{
-
-
-    kseq_t *seq = (kseq_t*)calloc(1, sizeof(kseq_t));	
-
-
-    long long read_number = 0;
-
-    HPC_seq HPC_read;
-    uint64_t code;
-    Hash_code k_code;
-    int avalible_k = 0;
-
-    fprintf(stdout, "Start Verifying ...\n");
-
-    while (get_read(seq))
-    {
-
-        init_HPC_seq(&HPC_read, seq->seq.s, seq->seq.l);
-        init_Hash_code(&k_code);
-
-        avalible_k = 0;
-
-        while ((code = get_HPC_code(&HPC_read)) != 6)
-        {
-            if(code < 4)
-            {
-                k_mer_append(&k_code,code,k_mer_length);
-                avalible_k++;
-                if (avalible_k>=k_mer_length)
-                {
-                    if(verify_Total_Count_Table(&TCB, &k_code, k_mer_length) == -1)
-                    {
-                        fprintf(stderr, "ERROR when subtracting!\n");
-                    }
-                }
-                
-            }
-            else
-            {
-                avalible_k = 0;
-                init_Hash_code(&k_code);
-            }
-            
-        }
-
-
-
-
-        read_number++;
-    }
-    
-    fprintf(stdout, "read_number: %lld\n",read_number);
-
-    fprintf(stdout, "Start Traversing ...\n");
-
-    Traverse_Total_Count_Table(&TCB);
-
-    fprintf(stdout, "Finish Traversing!\n");
-
-
-}
+Total_Pos_Table PCB;
+All_reads R_INF;
 
 
 
@@ -82,9 +17,6 @@ void Verify_Counting()
 void* Perform_Counting(void* arg)
 {
     int thr_ID = *((int*)arg);
-
-    ///debug_mode(101, thr_ID, thread_num);
-
 
     int i = 0;
     HPC_seq HPC_read;
@@ -157,9 +89,6 @@ void* Perform_Counting(void* arg)
     destory_R_buffer_block(&curr_sub_block);
     free(arg);
 
-    fprintf(stdout, "thr_ID: %d, read_number: %lld, select_k_mer_number: %lld, k_mer_number: %lld\n",
-    thr_ID, read_number, select_k_mer_number, k_mer_number);
-
 }
 
 
@@ -167,7 +96,94 @@ void* Perform_Counting(void* arg)
 
 
 
+void* Build_hash_table(void* arg)
+{
+    int thr_ID = *((int*)arg);
 
+    int i = 0;
+    HPC_seq HPC_read;
+
+    R_buffer_block curr_sub_block;
+
+    init_R_buffer_block(&curr_sub_block);
+
+	int file_flag = 1;
+
+    uint64_t code;
+
+    long long HPC_base;
+
+    Hash_code k_code;
+
+    int avalible_k = 0;
+
+    char* dest;
+
+	while (file_flag != 0)
+	{
+
+        file_flag = get_reads_mul_thread(&curr_sub_block);
+
+
+        for (i = 0; i < curr_sub_block.num; i++)
+        {
+
+           dest = R_INF.read+R_INF.index[curr_sub_block.read[i].ID];
+           memcpy(dest, curr_sub_block.read[i].seq.s, curr_sub_block.read[i].seq.l);
+
+           dest = R_INF.name+R_INF.name_index[curr_sub_block.read[i].ID];
+           memcpy(dest, curr_sub_block.read[i].name.s, curr_sub_block.read[i].name.l);
+            
+            
+            
+            init_HPC_seq(&HPC_read, curr_sub_block.read[i].seq.s, curr_sub_block.read[i].seq.l);
+            init_Hash_code(&k_code);
+
+            avalible_k = 0;
+
+            HPC_base = 0;
+
+            while ((code = get_HPC_code(&HPC_read)) != 6)
+            {
+                if(code < 4)
+                {
+                    k_mer_append(&k_code,code,k_mer_length);
+                    avalible_k++;
+                    if (avalible_k>=k_mer_length)
+                    {
+
+                       ///选取的k-mer满足两个要求
+                       ///1. hash(k-mer) % 101 <= 3
+                       ///2. occ(k-mer)要满足范围
+                       ///TCB表中的元素仅满足第一个要求，而PCB表中的元素满足两个要求
+                       ///所以如果当前k-mer在PCB表中存在，则他的位置一定要加入到候选位置中去
+                        insert_Total_Pos_Table(&PCB, &k_code, k_mer_length, 
+                            curr_sub_block.read[i].ID, HPC_base - k_mer_length + 1);
+                    }
+                    
+                }
+                else
+                {
+                    avalible_k = 0;
+                    init_Hash_code(&k_code);
+                }
+
+                HPC_base++;
+            }
+            
+            
+
+        }
+        
+
+    }
+
+    destory_R_buffer_block(&curr_sub_block);
+    free(arg);
+
+
+
+}
 
 
 
@@ -180,20 +196,24 @@ void* Perform_Counting(void* arg)
 void Counting_multiple_thr()
 {
 
+    double start_time = Get_T();
+
+    fprintf(stdout, "Begin Counting ...... \n");
+
+    init_kseq(read_file_name);
+
+    init_All_reads(&R_INF);
+
     init_Total_Count_Table(k_mer_length, &TCB);
-
-    fprintf(stdout, "TCB.prefix_bits: %d\n", TCB.prefix_bits);
-    fprintf(stdout, "TCB.suffix_bits: %d\n", TCB.suffix_bits);
-    fprintf(stdout, "TCB.size: %d\n", TCB.size);
-
-
 
     pthread_t inputReadsHandle;
     
     init_R_buffer(thread_num);
 
-    pthread_create(&inputReadsHandle, NULL, input_reads_muti_threads, NULL);
+    int *is_insert = (int*)malloc(sizeof(*is_insert));
+	*is_insert = 1;
 
+    pthread_create(&inputReadsHandle, NULL, input_reads_muti_threads, (void*)is_insert);
 
     pthread_t *_r_threads;
 
@@ -219,24 +239,299 @@ void Counting_multiple_thr()
 
     free(_r_threads);
 
-    destory_R_buffer();
-
-
-    destory_Total_Count_Table(&TCB);
+    ///destory_R_buffer();
     
+    ///destory_Total_Count_Table(&TCB);
+
+    destory_kseq();
+
+    fprintf(stdout, "Finish Counting ...... \n");
+
+    fprintf(stdout, "%-30s%18.2f\n\n", "Counting time:", Get_T() - start_time);
+   
 
 }
 
 
 
 
+void Build_hash_table_multiple_thr()
+{
+    double start_time = Get_T();
+
+
+    fprintf(stdout, "Begin Building hash table ...... \n");
+
+    init_Total_Pos_Table(&PCB, &TCB);
+
+    double T_start_time = Get_T();
+
+    Traverse_Counting_Table(&TCB, &PCB, k_mer_min_freq, k_mer_max_freq);
+
+    fprintf(stdout, "%-30s%18.2f\n\n", "Traverse time:", Get_T() - T_start_time);
+   
+
+
+    init_kseq(read_file_name);
+
+    clear_R_buffer();
+
+    malloc_All_reads(&R_INF);
+
+    pthread_t inputReadsHandle;
+
+    int *is_insert = (int*)malloc(sizeof(*is_insert));
+	*is_insert = 0;
+
+    pthread_create(&inputReadsHandle, NULL, input_reads_muti_threads, (void*)is_insert);
 
 
 
 
+    pthread_t *_r_threads;
+
+	_r_threads = (pthread_t *)malloc(sizeof(pthread_t)*thread_num);
+
+    int i = 0;
+
+	for (i = 0; i < thread_num; i++)
+	{
+		int *arg = (int*)malloc(sizeof(*arg));
+		*arg = i;
+
+		pthread_create(_r_threads + i, NULL, Build_hash_table, (void*)arg);
+
+	}
+
+    pthread_join(inputReadsHandle, NULL);
+
+    for (i = 0; i<thread_num; i++)
+		pthread_join(_r_threads[i], NULL);
+
+    free(_r_threads);
+
+    destory_kseq();
+
+    fprintf(stdout, "Finish Building hash table ...... \n");
+
+    fprintf(stdout, "%-30s%18.2f\n\n", "Build hash table time:", Get_T() - start_time);
+
+    ///destory_Total_Count_Table(&TCB);
+}
 
 
 
+
+/********************************for debug***************************************/
+void Verify_Counting()
+{
+
+
+    kseq_t *seq = (kseq_t*)calloc(1, sizeof(kseq_t));	
+
+
+    long long read_number = 0;
+
+    HPC_seq HPC_read;
+    uint64_t code;
+    Hash_code k_code;
+    int avalible_k = 0;
+
+    fprintf(stdout, "Start Verifying ...\n");
+
+    while (get_read(seq))
+    {
+
+        init_HPC_seq(&HPC_read, seq->seq.s, seq->seq.l);
+        init_Hash_code(&k_code);
+
+        avalible_k = 0;
+
+        while ((code = get_HPC_code(&HPC_read)) != 6)
+        {
+            if(code < 4)
+            {
+                k_mer_append(&k_code,code,k_mer_length);
+                avalible_k++;
+                if (avalible_k>=k_mer_length)
+                {
+                    if(verify_Total_Count_Table(&TCB, &k_code, k_mer_length) == -1)
+                    {
+                        fprintf(stderr, "ERROR when subtracting!\n");
+                    }
+                }
+                
+            }
+            else
+            {
+                avalible_k = 0;
+                init_Hash_code(&k_code);
+            }
+            
+        }
+
+
+
+
+        read_number++;
+    }
+    
+    fprintf(stdout, "read_number: %lld\n",read_number);
+
+    fprintf(stdout, "Start Traversing ...\n");
+
+    Traverse_Total_Count_Table(&TCB);
+
+    fprintf(stdout, "Finish Traversing!\n");
+
+
+}
+
+
+
+/********************************for debug*****************************************/
+void verify_Position_hash_table()
+{
+
+    init_kseq(read_file_name);
+
+    kseq_t *seq = (kseq_t*)calloc(1, sizeof(kseq_t));	
+
+
+    long long read_number = 0;
+    long long HPC_base;
+
+    HPC_seq HPC_read;
+    uint64_t code;
+    Hash_code k_code;
+    int avalible_k = 0;
+    k_mer_pos* list;
+    uint64_t sub_ID;
+
+    fprintf(stdout, "Start Verifying Position Table...\n");
+
+    while (get_read(seq))
+    {
+
+        if (seq->seq.l
+            != Get_READ_LENGTH(R_INF, read_number))
+        {
+            fprintf(stderr, "seq error\n");
+        }
+
+        if(memcmp(seq->seq.s, Get_READ(R_INF, read_number), seq->seq.l))
+        {
+            fprintf(stderr, "seq error\n");
+        }
+
+        if (seq->name.l
+            != Get_NAME_LENGTH(R_INF, read_number))
+        {
+            fprintf(stderr, "name error\n");
+        }
+
+
+         if(memcmp(seq->name.s, Get_NAME(R_INF, read_number), seq->name.l))
+        {
+            fprintf(stderr, "name error\n");
+        }
+
+
+
+        init_HPC_seq(&HPC_read, seq->seq.s, seq->seq.l);
+        init_Hash_code(&k_code);
+
+        avalible_k = 0;
+        HPC_base = 0;
+
+        while ((code = get_HPC_code(&HPC_read)) != 6)
+        {
+            if(code < 4)
+            {
+                k_mer_append(&k_code,code,k_mer_length);
+                avalible_k++;
+                if (avalible_k>=k_mer_length)
+                {
+
+                    uint64_t count1 = get_Total_Count_Table(&TCB, &k_code, k_mer_length);
+                    uint64_t count2 = count_Total_Pos_Table(&PCB, &k_code, k_mer_length);
+                    if(count1>=k_mer_min_freq && count1<= k_mer_max_freq && count1!=count2)
+                    {
+                        fprintf(stderr, "count1: %lld\n",count1);
+                        fprintf(stderr, "count2: %lld\n",count2);
+                    }
+                    
+
+                    if(locate_Total_Pos_Table(&PCB, &k_code, &list, k_mer_length, &sub_ID) == count2)
+                    {
+                        int j = 0;
+                        for(j=1; j<count2; j++)
+                        {
+                            if(list[j].readID < list[j - 1].readID)
+                            {
+                                fprintf(stderr, "locate error\n");
+                            }
+                            else if(list[j].readID == list[j - 1].readID)
+                            {
+                                if(list[j].offset < list[j - 1].offset)
+                                {
+                                    fprintf(stderr, "locate error\n");
+                                }
+                            }
+                        }
+
+                        if(count2 != 0)
+                        {
+                            for(j=0; j<count2; j++)
+                            {   
+                                if(list[j].readID == read_number && list[j].offset == HPC_base - k_mer_length + 1)
+                                {
+                                    break;
+                                }
+                            }
+
+                            if(j == count2)
+                            {
+                                fprintf(stderr, "locate error, read_number: %llu, pos: %llu\n", 
+                                read_number, HPC_base - k_mer_length + 1);
+
+                                for(j=0; j<count2; j++)
+                                {   
+
+                                    fprintf(stderr, "j: %llu, readID: %llu, offset: %llu\n", j, list[j].readID, list[j].offset);
+                            
+                                }
+                            }
+                        }
+                        
+                        
+                    }
+                    else
+                    {
+                        fprintf(stderr, "locate error\n");
+                    }
+
+                }
+                
+            }
+            else
+            {
+                avalible_k = 0;
+                init_Hash_code(&k_code);
+            }
+
+            HPC_base++;
+            
+        }
+
+        read_number++;
+    }
+
+    destory_kseq();
+
+    fprintf(stdout, "Finish Verifying Position Table!\n");
+
+}
 
 
 
--- Process_Read.h
+++ Process_Read.h
@@ -7,11 +7,17 @@
 #include <zlib.h>
 #include "kseq.h"
 
+#define READ_INIT_NUMBER 1000
+
 #define READ_BLOCK_SIZE 64
 #define READ_BLOCK_NUM_PRE_THR 100
 
 #define IS_FULL(buffer) ((buffer.num >= buffer.size)?1:0)
 #define IS_EMPTY(buffer) ((buffer.num == 0)?1:0)
+#define Get_READ_LENGTH(R_INF, ID) (R_INF.index[ID+1] - R_INF.index[ID])
+#define Get_NAME_LENGTH(R_INF, ID) (R_INF.name_index[ID+1] - R_INF.name_index[ID])
+#define Get_READ(R_INF, ID) (R_INF.read + R_INF.index[ID])
+#define Get_NAME(R_INF, ID) (R_INF.name + R_INF.name_index[ID])
 
 
 
@@ -22,6 +28,24 @@ void destory_kseq();
 int get_read(kseq_t *s);
 
 
+
+typedef struct
+{
+	char* read;
+	char* name;
+	uint64_t* index;
+	uint64_t index_size;
+	uint64_t* name_index;
+	uint64_t name_index_size;
+	uint64_t total_reads;
+	uint64_t total_reads_bases;
+	uint64_t total_name_length;
+
+} All_reads;
+
+extern All_reads R_INF;
+
+void malloc_All_reads(All_reads* r);
 
 typedef struct
 {
@@ -41,6 +65,7 @@ typedef struct
 } R_buffer;
 
 void init_R_buffer(int thread_num);
+void init_All_reads(All_reads* r);
 void* input_reads_muti_threads(void*);
 void init_R_buffer_block(R_buffer_block* curr_sub_block);
 int get_reads_mul_thread(R_buffer_block* curr_sub_block);
@@ -48,5 +73,7 @@ int get_reads_mul_thread(R_buffer_block* curr_sub_block);
 void Counting_block();
 void destory_R_buffer_block(R_buffer_block* curr_sub_block);
 void destory_R_buffer();
+void clear_R_buffer();
+
 
 #endif
