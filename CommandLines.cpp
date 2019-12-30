1e71947a239d0e4eab710e705e0e44370e939300
Heng Li <lh3@me.com>
2019-12-30 12:20:07 -0500

    bumped version number to 0.1.0

--- CommandLines.cpp
+++ CommandLines.cpp
@@ -5,7 +5,7 @@
 #include "ketopt.h"
 #include <sys/time.h>
 
-#define VERSION "0.0.0.1"
+#define VERSION "0.1.0"
 #define DEFAULT_OUTPUT "hifiasm.asm"
 
 hifiasm_opt_t asm_opt;
@@ -28,7 +28,7 @@ void Print_H(hifiasm_opt_t* asm_opt)
     fprintf(stderr, "    -k INT        k-mer length [%d] (must be < 64)\n", asm_opt->k_mer_length);
     ///fprintf(stderr, "    -w            write all overlaps to disk, can accelerate assembly next time [%d]\n", asm_opt->write_index_to_disk);
     ///fprintf(stderr, "    -l            load all overlaps from disk, can avoid overlap calculation [%d]\n", asm_opt->load_index_from_disk);
-    fprintf(stderr, "    -i            do the overlap calculation even if there are overlaps of previous run on disk.\n");
+    fprintf(stderr, "    -i            ignore saved overlaps in *.ovlp*.bin files\n");
     fprintf(stderr, "    -z INT        length of adapters that should be removed [%d]\n", asm_opt->adapterLen);
     fprintf(stderr, "    -p INT        size of popped bubbles [%lld]\n", asm_opt->pop_bubble_size);
     fprintf(stderr, "    -x FLOAT      max overlap drop ratio [%.2g]\n", asm_opt->max_drop_rate);
